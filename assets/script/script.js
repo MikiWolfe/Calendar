0344ea2ef8c77b9b4f4405c86d4f34aa93f16791
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-02 14:43:41 -0800

    able to display button and text on the screen

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,5 @@
 var buisnessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]; // array for hours
-var currentHour = moment().hour(); // how to know what the current hour is 
-
+var currentHour = moment().hour(); // how to know what the current hour is
 
 function displayCurrentDate() {
   var currentDay = moment().format("dddd, MMMM Do YYYY");
@@ -10,7 +9,8 @@ function displayCurrentDate() {
 function displayDayPlanner() {
   for (let i = 0; i < buisnessHours.length; i++) {
     let row = $("<div class='row'>");
-    let colHr = $("<div class='col-sm-2'>");
+    let colHr = $("<div class='col-med-1'>");
+  
 
     let hours = buisnessHours[i] + "AM"; // coverting array into hours
     if (buisnessHours[i] >= 12) {
@@ -19,25 +19,23 @@ function displayDayPlanner() {
         hours = buisnessHours[i] - 12 + "PM";
       }
     }
+    let saveBtn = $("<button class = 'btn saveBtn col-md-1'>");
+    let textArea= $("<textarea class='col-med-10'>")
     colHr.append(hours);
-    row.append(colHr);
+    row.append(colHr,saveBtn,textArea);
     $(".container").append(row); // appending hours to html
-  }
-  function hourPlan() {
-    for (let i = 0; i < currentHour.length; index++) {
-      let = $("<div id ='hour-9' class= 'row time-block past'>");
-    }
+    // $(".row").append(saveBtn);
+    // $(".row").append(textArea)
+    
   }
 }
 
-$('.container').on('click','button', function saveDay(event){
- (event.target)
-})
+$(".container").on("click", "button", function saveDay(event) {
+  event.target;
+});
 
 displayDayPlanner();
 displayCurrentDate();
-
-
 
 /* text area and append the text area with save button 
 
