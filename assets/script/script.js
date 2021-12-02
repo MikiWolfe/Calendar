07e8aa0d817ab6845ad4acc0357fb696a0cc5ade
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-02 13:47:51 -0800

    trying to use jquary to creat new elements

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,33 +1,46 @@
-var buisnessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
-var currentHour = moment().hour();
+var buisnessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]; // array for hours
+var currentHour = moment().hour(); // how to know what the current hour is 
 
 
- function displayCurrentDate() {
+function displayCurrentDate() {
   var currentDay = moment().format("dddd, MMMM Do YYYY");
-  $("#currentDay").append(currentDay)
-  
-} 
+  $("#currentDay").append(currentDay); // using moment to display the day of the week and date
+}
 
 function displayDayPlanner() {
   for (let i = 0; i < buisnessHours.length; i++) {
     let row = $("<div class='row'>");
     let colHr = $("<div class='col-sm-2'>");
 
-    let hours = buisnessHours[i] + "AM";
+    let hours = buisnessHours[i] + "AM"; // coverting array into hours
     if (buisnessHours[i] >= 12) {
       hours = buisnessHours[i] + "PM";
       if (buisnessHours[i] >= 13) {
         hours = buisnessHours[i] - 12 + "PM";
       }
     }
-
     colHr.append(hours);
     row.append(colHr);
-    $("#dayPlanner").append(row);
+    $(".container").append(row); // appending hours to html
+  }
+  function hourPlan() {
+    for (let i = 0; i < currentHour.length; index++) {
+      let = $("<div id ='hour-9' class= 'row time-block past'>");
+    }
   }
 }
 
-// localStorage.setItem( 'hour-9', "hour 9 text value");
+$('.container').on('click','button', function saveDay(event){
+ (event.target)
+})
+
 displayDayPlanner();
 displayCurrentDate();
-/* text area and append the text area with save button */
+
+
+
+/* text area and append the text area with save button 
+
+Local Storage:
+localStorage.setItem("hour-9", "hour 9 text value");
+var savedValueForHour = localStorage.getItem("hour-"+ i) */
