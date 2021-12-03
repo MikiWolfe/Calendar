7c4c0e54408eae681d2b6e4f7beca88538dba886
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-03 15:46:57 -0800

    struggling with getting hours to display

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,7 @@
 var buisnessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]; // array for hours
 var currentHour = moment().hour(); // how to know what the current hour is
 
-
-  // using moment to display the day of the week and date
+// using moment to display the day of the week and date
 function displayCurrentDate() {
   var currentDay = moment().format("dddd, MMMM Do YYYY");
   $("#currentDay").append(currentDay);
@@ -25,16 +24,32 @@ function displayDayPlanner() {
     row.append(colHr, textArea, saveBtn);
     $(".container").append(row); // appending hours to html
   }
+
   $(".container").on("click", "button", function saveDay(event) {
-    var time = $(this).parent().attr("container");
-    var text = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id");
+    var text = $(this).siblings(".description").val();
     event.target.localStorage.setItem(time, text);
-  })
-  
-  
+  });
   }
 
-
+  function displayColor(){
+  $(".time-block").each(function () {
+    var timeBlock = 0;
+    if (timeBlock < currentHour) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (timeBlock === currentHour) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future")
+    }
+  }); console.log(currentHour)
+  }
 
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
@@ -46,7 +61,6 @@ $("#hour-15 .description").val(localStorage.getItem("hour-15"));
 $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-
 displayDayPlanner();
 displayCurrentDate();
-
+displayColor();
