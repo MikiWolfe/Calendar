3b0431f53864019d77f41359015a57a69533c251
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-03 15:16:09 -0800

    additions for local storage

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,49 +1,52 @@
 var buisnessHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]; // array for hours
 var currentHour = moment().hour(); // how to know what the current hour is
 
+
+  // using moment to display the day of the week and date
 function displayCurrentDate() {
   var currentDay = moment().format("dddd, MMMM Do YYYY");
-  $("#currentDay").append(currentDay); // using moment to display the day of the week and date
+  $("#currentDay").append(currentDay);
 }
 
 function displayDayPlanner() {
   for (let i = 0; i < buisnessHours.length; i++) {
-    let row = $("<div class='row'>");
-    let colHr = $("<div class='col-med-1 hour'>");
-
+    let row = $("<div class='row time-block'>");
+    let colHr = $("<div class='col-med-2 hour'>");
     let hours = buisnessHours[i] + "AM"; // coverting array into hours
     if (buisnessHours[i] >= 12) {
       hours = buisnessHours[i] + "PM";
       if (buisnessHours[i] >= 13) {
         hours = buisnessHours[i] - 12 + "PM";
       }
-      if (hours === currentHour) {
-        document.getElementByClass("row").style.display = "present";
-      }
-      if (hours >= currentHour) {
-        document.getElementsByClassName("row").style.display = "past";
-      }
-      if (hours <= currentHour) {
-        document.getElementsByClassName("row").style.display = "future";
-      }
     }
-    
-
-    let saveBtn = $("<button class = 'btn saveBtn col-md-1'>");
-    let textArea = $("<textarea class='col-med-10 description'>");
+    var textArea = $("<textarea class='col-med-10 description'>");
+    var saveBtn = $("<button class = 'btn saveBtn col-md-1'>");
     colHr.append(hours);
     row.append(colHr, textArea, saveBtn);
     $(".container").append(row); // appending hours to html
   }
-}
+  $(".container").on("click", "button", function saveDay(event) {
+    var time = $(this).parent().attr("container");
+    var text = $(this).siblings(".description").val()
+    event.target.localStorage.setItem(time, text);
+  })
+  
+  
+  }
 
-$(".container").on("click", "button", function saveDay(event) {
-  event.target;
-});
+
+
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
 
 displayDayPlanner();
 displayCurrentDate();
 
-/* Local Storage:
-localStorage.setItem("hour-9", "hour 9 text value");
-var savedValueForHour = localStorage.getItem("hour-"+ i) */
