3291bdba085abffbb4e22bacb45dd4f8a2ec86d1
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-05 16:09:25 -0800

    trying to get the layout right

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,22 +1,32 @@
 //  = [9, 10, 11, 12, 13, 14, 15, 16, 17]; // array for hours
 var currentHour = moment().hours(); // how to know what the current hour is
-var buisnessHours = document.getElementsByid("#container");
+// var buisnessHours = document.getElementsByid("container");
 // using moment to display the day of the week and date
 function displayCurrentDate() {
   var currentDay = moment().format("dddd, MMMM Do YYYY");
   $("#currentDay").append(currentDay);
 }
-// appending hours to html
-function displayDayPlanner() {
-  var row = $("class='row time-block'>");
+
+$("#container").addClass("row time-block col-md-1 hour");
+$("#hour-10").addClass("row time-block col-md-1 hour");
+
+
+
+
+/*
+
+var row = $("class='row time-block'>");
   var colHr = $("class='hour col-med-3'>");
   var textArea = $("class= 'description col-sm-10'>");
   var saveBtn = $("class = 'btn saveBtn col-sm-1'>");
+
+  // appending hours to html
+function displayDayPlanner() {
+  
   // colHr.append(buisnessHours);
   row.append(colHr, textArea, saveBtn);
-  $(".container").append(row);
-
-  /*for (let i = 0; i < buisnessHours.length; i++) {
+  $("#container").append(row);}
+for (let i = 0; i < buisnessHours.length; i++) {
   
     let hours = buisnessHours[i] + "AM"; // coverting array into hours
     if (buisnessHours[i] >= 12) {
@@ -26,13 +36,13 @@ function displayDayPlanner() {
       }
     }
   } */
-}
+
 
 /*$(".container").on("click", "button", (event) => {
     event.target.localStorage.setItem("#hour-9", ".description");
     console.log(localStorage);
   });
-} */
+} 
 
 function displayColor() {
   $(".time-block").each(function () {
@@ -58,5 +68,7 @@ $("#hour-16 .description").val(localStorage.getItem("hour-16"));
 $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 displayDayPlanner();
-displayCurrentDate();
+
 displayColor();
+*/
+displayCurrentDate();
