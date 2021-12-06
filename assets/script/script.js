bb843a1128e5e83bb8bec70cc111b3135284e00a
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-05 21:46:08 -0800

    added a button to clear storage

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
   // using the CSS provided so past is grey, present is red and future is green
 
   $(".time-block").each(function () {
-    var timeBlock = parseInt($(this).attr("id").split("hour")[1])
+    var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
     if (timeBlock < currentHour) {
       $(this).addClass("past");
     } else if (timeBlock === currentHour) {
@@ -21,21 +21,23 @@ $(document).ready(function () {
   });
 
   $(".saveBtn").on("click", function (event) {
-    event.preventDefault();
-   var quest= $(this).siblings(".description").val();
-   var time =$(this).parent().attr("id");
-   console.log(this)
-    localStorage.setItem(time, quest);
+    // event.preventDefault();
+    var quest = $(this).siblings(".description").val();
+    var hour = $(this).parent().attr("id");
+    localStorage.setItem(hour, quest);
   });
 
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-  
+});
+$("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
+
+$(".btn-danger").on("click", function () {
+  localStorage.clear();
 });
