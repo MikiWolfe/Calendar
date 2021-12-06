2334261f4ff9f93480529a616abd3005642ce48a
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-05 17:48:34 -0800

    trying to get quests for the day saved into local storage

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,3 @@
-
 var currentHour = moment().hours(); // how to know what the current hour is
 
 // using moment to display the day of the week and date
@@ -7,6 +6,7 @@ function displayCurrentDate() {
   $("#currentDay").append(currentDay);
 }
 displayCurrentDate();
+
 // using the CSS provided so past is grey, present is red and future is green
 function displayColor() {
   $(".time-block").each(function () {
@@ -21,24 +21,11 @@ function displayColor() {
   });
 }
 displayColor();
-function saveTheDay(){
-
-(".button").on("click", "button", (event) => {
-    event.target.localStorage.setItem("#hour-9", ".description");
-    console.log(localStorage);
-  });
-} 
-
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
-
-
-
-
+function saveTheDay (){
+$(".saveBtn").on("click", function () {
+  var quest = $(this).sibling(".description").val();
+  var hour = $(this).parent().attr("id");
+    localStorage.setItem(quest, hour);
+})}
+saveTheDay()
+$("#hour-9 .description").val(localStorage).getItem("hour-9")
