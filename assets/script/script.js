74d767898f02dd40e7892b364fc3d5ab0b32d73f
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-05 20:00:07 -0800

    got the local storage to work!

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,31 +1,33 @@
-var currentHour = moment().hours(); // how to know what the current hour is
+var currentHour = moment().hour(); // how to know what the current hour is
+$(document).ready(function () {
+  // using moment to display the day of the week and date
 
-// using moment to display the day of the week and date
-function displayCurrentDate() {
   var currentDay = moment().format("dddd, MMMM Do YYYY");
   $("#currentDay").append(currentDay);
-}
-displayCurrentDate();
 
-// using the CSS provided so past is grey, present is red and future is green
-function displayColor() {
+  // using the CSS provided so past is grey, present is red and future is green
+
   $(".time-block").each(function () {
-    var time = parseInt($(this).attr("id").split("hour")[1]);
-    if (time < currentHour) {
+    var timeBlock = $(this).attr("id").split("-")[1];
+    if (timeBlock < currentHour) {
       $(this).addClass("past");
-    } else if (time === currentHour) {
+    } else if (timeBlock === currentHour) {
+      $(this).removeClass("past");
       $(this).addClass("present");
     } else {
+      $(this).removeClass("present");
       $(this).addClass("future");
     }
   });
-}
-displayColor();
-function saveTheDay (){
-$(".saveBtn").on("click", function () {
-  var quest = $(this).sibling(".description").val();
-  var hour = $(this).parent().attr("id");
-    localStorage.setItem(quest, hour);
-})}
-saveTheDay()
-$("#hour-9 .description").val(localStorage).getItem("hour-9")
+
+  $(".saveBtn").on("click", function (event) {
+    event.preventDefault();
+   var quest= $(this).siblings(".description").val();
+   var time =$(this).parent().attr("id");
+   console.log(this)
+    localStorage.setItem(time, quest);
+  });
+
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+});
