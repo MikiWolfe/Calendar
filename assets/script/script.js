d8cfd156c58adeccbc1d813976460fdb4c5cc63f
Mikayla Bruce <mkla.w.b@gmail.com>
2021-12-05 20:42:12 -0800

    trying to add a delete button for the fun of it. vs code is crashing so I'm commiting

--- assets/script/script.js
+++ assets/script/script.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
   // using the CSS provided so past is grey, present is red and future is green
 
   $(".time-block").each(function () {
-    var timeBlock = $(this).attr("id").split("-")[1];
+    var timeBlock = parseInt($(this).attr("id").split("hour")[1])
     if (timeBlock < currentHour) {
       $(this).addClass("past");
     } else if (timeBlock === currentHour) {
@@ -28,6 +28,14 @@ $(document).ready(function () {
     localStorage.setItem(time, quest);
   });
 
-  $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+  
 });
